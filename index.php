6392e2d45b46b39f9c19fb43391af721efc5de63
jatinag17 <jatinagrawal128@gmail.com>
2025-03-28 00:34:42 +0530

    Remove commented-out user variable outputs and add arithmetic operator examples in index.php

--- index.php
+++ index.php
@@ -16,29 +16,54 @@
 - boolean
 
 */
-$name="John Doe"; //string
-$age=30; //integer
-$height=5.7; //float
-$isMale=true; //boolean
+// $name="John Doe"; //string
+// $age=30; //integer
+// $height=5.7; //float
+// $isMale=true; //boolean
 
 
-echo "My name is:".$name;
-echo "<br>";
-echo "</br>My age is:".$age;
-echo "<br>";
-echo "</br>My height is:".$height;
-echo "<br>";
-echo "</br>Am I male:".$isMale;
-echo "<br>";
-echo "<br>";
-var_dump($name);
-echo "<br>";
-echo "<br>";
-var_dump($age);
-echo "<br>";
-echo "<br>";
-var_dump($height);
-echo "<br>";
-echo "<br>";
-var_dump($isMale);
+// echo "My name is:".$name;
+// echo "<br>";
+// echo "</br>My age is:".$age;
+// echo "<br>";
+// echo "</br>My height is:".$height;
+// echo "<br>";
+// echo "</br>Am I male:".$isMale;
+// echo "<br>";
+// echo "<br>";
+// var_dump($name);
+// echo "<br>";
+// echo "<br>";
+// var_dump($age);
+// echo "<br>";
+// echo "<br>";
+// var_dump($height);
+// echo "<br>";
+// echo "<br>";
+// var_dump($isMale);
+
+
+/*
+## Arithmetic operators
+- Addition +
+- Subtraction -
+- Multiplication *
+- Division /
+- Modulus %
+- Exponentiation **
+
+## increment and decrement operators
+- Increment ++
+- Decrement --
+## operator precedence
+- Parentheses ()
+- Exponentiation **
+- Multiplication and Division *
+- Addition and Subtraction +
+
+*/
+$a=10;
+$b=3;
+echo $a+$b;
+
 ?>
