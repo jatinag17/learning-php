594441ca37b63db766e48d9ee41da33e30966040
jatinag17 <jatinagrawal128@gmail.com>
2025-03-28 00:22:34 +0530

    Refactor index.php to include variable definitions and output a name

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<?php
- echo "hello";
- echo "<h2> hello </h2>";
- ?>
+ <?php
+
+
+//  echo "hello";
+//  echo "<h2> hello </h2>";
+
+
+/*
+## variables
+ A reusable container that holds the data
+
+## Data types
+- string
+- integer
+- float
+- boolean
+
+*/
+$name="John Doe"; //string
+$age=30; //integer
+$height=5.7; //float
+$isMale=true; //boolean
+
+
+echo $name;
+?>
