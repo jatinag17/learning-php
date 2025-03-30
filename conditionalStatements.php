ac324b6d2e3663d908268a8637e0823d12967c90
jatinag17 <jatinagrawal128@gmail.com>
2025-03-30 21:03:05 +0530

    Update conditionalStatements.php to include logical operators and modify temperature condition example

--- conditionalStatements.php
+++ conditionalStatements.php
@@ -8,14 +8,28 @@
 // >= (greater than or equal to)
 // <= (less than or equal to)
 // == (equal to equal to)
-/*!(not) */
 
-$age=18;
-if ($age > 18) {
-    echo "You are an adult";
-} elseif ($age == 18) {
-    echo "You are 18 years old";
-} else {
-    echo "You are a minor";
+
+// $age=18;
+// if ($age > 18) {
+//     echo "You are an adult";
+// } elseif ($age == 18) {
+//     echo "You are 18 years old";
+// } else {
+//     echo "You are a minor";
+// }
+
+## Logical Operators
+// && (and)
+// || (or)
+/* ! (not) */
+
+$temp=45;
+ if ($temp > 30 && $temp < 50) {
+   echo "It's a hot day";
+} else{
+    echo "It's a nice day";
 }
+
+
 ?>
